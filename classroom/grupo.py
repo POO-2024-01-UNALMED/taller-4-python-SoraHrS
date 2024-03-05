eff2cbd921b5f50470a55eb987194248ebf58e05
Sorany <159492562+SoraHrS@users.noreply.github.com>
2024-03-05 11:32:55 -0500

    Actualizar 2 grupo.py

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,28 +1,35 @@
 from classroom.asignatura import Asignatura
-
 class Grupo:
-    grado = 12
+    grado = "Grado 12"
 
     def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
-        self._grupo = grupo
-        self._asignaturas = asignaturas or [] 
-        self.listadoAlumnos = estudiantes or [] 
+        self._grupo= grupo
+        self._asignaturas= asignaturas or []
+        self.listadoAlumnos=estudiantes or []
 
     def listadoAsignaturas(self, **args):
         for x in args.values():
-             A = Asignatura(x)
-       self._asignaturas.append(A)
+            asign=Asignatura(x)
+            self._asignaturas.append(asign)
 
     def agregarAlumno(self, alumno, lista=None):
-        if type(lista)!=list:
+        if type(lista) !=list:
             self.listadoAlumnos.append(alumno)
-        else: 
-           lista.append(alumno)
-           self.listadoAlumnos +lista
+        else:
+            lista.append(alumno)
+            self.listadoAlumnos+=lista
 
     def __str__(self):
-        return "Grupo de estudiantes: " +self._grupo
+        return "Grupo de estudiantes: " + self._grupo
+
+    # @ classmethod
+    # def asignarNombre(cls, nombre="Grado 10"):
+    #     cls.grado = nombre
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
+
+    # @ classmethod
+    # def asignarNombre(cls, nombre="Grado 4"):
+    #     cls.grado = nombre
