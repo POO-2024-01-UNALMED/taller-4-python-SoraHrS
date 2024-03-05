bf06096e04e8458a84b10fc94d486dba6b3dc5cf
Sorany <159492562+SoraHrS@users.noreply.github.com>
2024-03-05 11:26:25 -0500

    Actualizar grupo.py

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -3,32 +3,25 @@ from classroom.asignatura import Asignatura
 class Grupo:
     grado = 12
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=None):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
-        if asignaturas is None:
-            asignaturas = []
-            self._asignaturas = asignaturas
-        else:
-            self._asignaturas = asignaturas
-       
-        if estudiantes is None:
-            estudiantes = []
-            self.listadoAlumnos = estudiantes
-        else:
-            self.listadoAlumnos = estudiantes
+        self._asignaturas = asignaturas or [] 
+        self.listadoAlumnos = estudiantes or [] 
 
-    def listadoAsignaturas(self, kwargs):
-        for x in kwargs.values():
-            self._asignaturas.append(Asignatura(x))
+    def listadoAsignaturas(self, **args):
+        for x in args.values():
+             A = Asignatura(x)
+       self._asignaturas.append(A)
 
     def agregarAlumno(self, alumno, lista=None):
-        if lista is None:
-            lista = []
-        lista.append(alumno)
-        self.listadoAlumnos = self.listadoAlumnos.append(lista)
+        if type(lista)!=list:
+            self.listadoAlumnos.append(alumno)
+        else: 
+           lista.append(alumno)
+           self.listadoAlumnos +lista
 
     def __str__(self):
-        return "Grupo de estudiantes: grupo predeterminado"
+        return "Grupo de estudiantes: " +self._grupo
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 6"):
